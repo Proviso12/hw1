3fcc364135efa64c33c22ba80f8058ed41241342
Dariel Ravelo-Ramos <15proviso@gmail.com>
2024-02-17 17:39:01 -0500

    am able to print out the information from the json file. still cant get everything

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,29 @@ import "./card-layout.css";
 import "./styles.css";
 import { CardSuit } from './CardSuit'
 import internal from "stream";
-
-const dndDatabaseUrl = "https://www.dnd5eapi.co/api/"
-
+import { json } from "stream/consumers";
 
 //API being used: D&D 5E API
 //link to the API docs : https://5e-bits.github.io/docs/
 // API Tutorials: https://5e-bits.github.io/docs/docs/tutorials
+
+const dndDatabaseUrl = "https://www.dnd5eapi.co/api/"
 
 // Cards
 const raceCard = document.querySelector("#Races #card_Type") as HTMLButtonElement;
 const classCard = document.querySelector("#Classes #card_Type") as HTMLButtonElement;
 const spellsCard = document.querySelector("#Spells #card_Type") as HTMLButtonElement;
 const monstersCard = document.querySelector("#Monsters #card_Type") as HTMLButtonElement;
+const cardList = document.querySelector("#card_List") as HTMLUListElement;
 
+// Dice related stuff
+const diceAmount = document.querySelector('#dice-Amount') as HTMLInputElement;
+const diceType = document.querySelector("#dice-type") as HTMLSelectElement;
+const diceResult = document.querySelector('#dice-Result') as HTMLUListElement;
+document.querySelector('#roll-Button')?.addEventListener('click', (e: Event) => RollDice());
 
-const cardHandler = async (e: MouseEvent) => {
-  const apiQuery = (e.target as HTMLButtonElement).textContent?.toLowerCase() ?? '';
-
-  // this is what you'll be changing to match your website's needs
-  console.log(await SearchDndDatabase(apiQuery));
-}
-
-const setupCards = (): void => {
+//sets up each card to have their own onclick handler
+function setupCards(): void{
   raceCard.addEventListener('click', cardHandler);
   classCard.addEventListener('click', cardHandler);
   spellsCard.addEventListener('click', cardHandler);
@@ -34,44 +34,15 @@ const setupCards = (): void => {
 }
 setupCards();
 
-// Dice related stuff
-let diceAmount = document.querySelector('#dice-Amount') as HTMLInputElement;
-let diceResult = document.querySelector('#dice-Result') as HTMLUListElement;
-let diceType = document.querySelector("#dice-type") as HTMLSelectElement;
-document.querySelector('#roll-Button')?.addEventListener('click', (e: Event) => RollDice());
+//will respond to a button click by calling the api 
+//search function with the information of the clicked card
+async function cardHandler(e: MouseEvent) {
+  const apiQuery = (e.target as HTMLButtonElement).textContent?.toLowerCase() ?? '';
 
-
-
-function RollDice(): void {
-  let amount = Math.min(Number(diceAmount?.value), 10000);
-  let type = Number(diceType?.value);
-  let diceList: number[] = [];
-
-  // make sure the user knows that it's max 10000
-  diceAmount.value = amount.toString();
-
-  for (let i = 0; i < amount; i++) {
-    diceList.push(Math.floor(Math.random() * type + 1));
-    console.log(diceList[i]);
-  }
-  PrintDice(diceList);
+  // this is what you'll be changing to match your website's needs
+  console.log(await SearchDndDatabase(apiQuery));
+  PrintCards(AddNamesToList(await SearchDndDatabase(apiQuery)));
 }
-
-function PrintDice(diceList: any[]): void {
-  while (diceResult.firstChild) {
-    diceResult.removeChild(diceResult.lastChild as Node);
-  }
-  diceList.forEach(element => {
-    let node = document.createElement("li");
-
-    node.appendChild(document.createTextNode(element));
-    node.setAttribute("id", "book_Result");
-
-    diceResult.appendChild(node);
-  });
-}
-
-
 
 /**
  * Search DnD database
@@ -123,4 +94,60 @@ const SearchDndDatabase = async (query: string): Promise<object> => {
   let response = await fetch(`${dndDatabaseUrl}${query}`, fetchData);
   data = await response.json();
   return data;
+}
+
+function AddNamesToList(apiList: any): string[]
+{
+  let nameList:string[] = [];
+  for (let name of apiList.results) {
+   
+     console.log(JSON.stringify(name));
+     nameList.push(JSON.stringify(name));
+   }
+   return nameList;
+}
+
+function PrintCards(nameList: any): void {
+  cardList.innerHTML = "";
+
+  let node = document.createElement("li");
+  let string ="I recommend using my good ol reliable for your next champaign: ";
+  let recommend = nameList[Math.floor(Math.random()*9)];
+
+  node.appendChild(document.createTextNode(string + recommend))
+  cardList.appendChild(node);
+
+  nameList.forEach(element => {
+    node.appendChild(document.createTextNode(element));
+  });
+  cardList.appendChild(node);
+}
+
+//function which will gather the amount of dice that 
+//the user wants to roll and the type. Then it will proceed to roll that many times.
+function RollDice(): void {
+  let amount = Math.min(Number(diceAmount?.value), 10000);
+  let type = Number(diceType?.value);
+  let diceList: number[] = [];
+
+  // make sure the user knows that it's max 10000
+  diceAmount.value = amount.toString();
+
+  for (let i = 0; i < amount; i++) {
+    diceList.push(Math.floor(Math.random() * type + 1));
+    console.log(diceList[i]);
+  }
+  PrintDice(diceList);
+}
+
+//displays the results of the dice roll by adding it to an empty list element
+function PrintDice(diceList: any[]): void {
+  diceResult.innerHTML = "";
+
+  diceList.forEach(element => {
+    let node = document.createElement("li");
+
+    node.appendChild(document.createTextNode(element));
+    diceResult.appendChild(node);
+  });
 }
